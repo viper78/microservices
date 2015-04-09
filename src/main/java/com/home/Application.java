06ad8dd03037250bc1caab24adf0e5e49e7a7d2b
viper78 <rajeshs.mail@gmail.com>
2015-04-09 10:39:29 -0700

    commit 2

--- src/main/java/com/home/Application.java
+++ src/main/java/com/home/Application.java
@@ -4,4 +4,7 @@ package com.home;
  * Created by Rajesh Subramanian on 4/9/15.
  */
 public class Application {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
